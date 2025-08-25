ad56fff2896a291cef084fddeae864e44a1a3f21
aaron <aaron.aa@me.com>
2025-08-25 16:19:16 +0200

    Adde vite env var

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -2,6 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_SANITY_PROJECT_ID: string;
   readonly VITE_SANITY_DATASET: string;
   readonly VITE_SANITY_TOKEN: string;
+  readonly VITE_GOOGLE_RECAPTCHA_V3_SITE_KEY: string;
 }
 
 interface ImportMeta {
